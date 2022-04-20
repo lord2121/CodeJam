0e63a2a6b937f21cee19d9f2ecf0906ebbddc438
AndreiMaces <80191286+AndreiMaces@users.noreply.github.com>
2022-04-20 21:34:47 +0300

    .

--- index.js
+++ index.js
@@ -4,26 +4,27 @@ const PORT = 3000
 const mongoose = require("mongoose")
 const ejsMate = require("ejs-mate")
 const productRoutes = require('./routes/productRoutes.js')
+const path = require('path')
 
 
 
 const app = express()
-app.engine('ejs' , ejsMate);
-app.set("view engine" , "ejs")
-app.use(express.static("public"))
+app.engine('ejs', ejsMate);
+app.set("view engine", "ejs")
+app.use(express.static(path.join(__dirname, 'public')))
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render("index")
 })
 
-app.use('/product' , productRoutes);
+app.use('/product', productRoutes);
 
 
-app.use('*' , (req , res) => {
+app.use('*', (req, res) => {
     res.send("404 not found")
 })
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log("Listening on PORT 3000")
 })
