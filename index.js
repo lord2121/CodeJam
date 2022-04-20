b044964a8caffb9153fc5a532a0352b59769bf01
lord2121 <53796515+lord2121@users.noreply.github.com>
2022-04-20 23:15:19 +0300

    .

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose")
 const ejsMate = require("ejs-mate")
 const productRoutes = require('./routes/productRoutes.js')
 const productModel = require('./models/productModel.js');
+const passport = require("passport")
+const FacebookStrategy = require('passport-facebook').Strategy;
 
 
 
@@ -12,6 +14,28 @@ const app = express()
 app.engine('ejs' , ejsMate);
 app.set("view engine" , "ejs")
 app.use(express.static("public"))
+
+passport.use(new FacebookStrategy({
+    clientID: 366825112153917,
+    clientSecret: "537e5fbf5466b8032d793d802fbaf427",
+    callbackURL: "http://localhost:3000/auth/facebook/bravisimoi"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/bravisimoi',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
 
 
 app.get("/",(req,res)=>{
@@ -30,6 +54,10 @@ app.get("/product/:id",(req,res)=>{
 
 app.get("/product/post",(req,res)=>{
     res.render("../views/product/productForm.ejs");
+})
+
+app.get("/login",(req,res)=>{
+    res.render("../views/partials/login.ejs");
 })
 
 
