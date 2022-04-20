e0fcd97d6089312b58e299a7f69d5589e9cb5216
lord2121 <53796515+lord2121@users.noreply.github.com>
2022-04-20 21:40:00 +0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const ejs = require("ejs")
+const PORT = 3000
+const mongoose = require("mongoose")
+const ejsMate = require("ejs-mate")
+const productRoutes = require('./routes/productRoutes.js')
+const productModel = require('./models/productModel.js');
+
+
+
+const app = express()
+app.engine('ejs' , ejsMate);
+app.set("view engine" , "ejs")
+app.use(express.static("public"))
+
+
+app.get("/",(req,res)=>{
+    res.render("index")
+})
+
+
+
+app.get("/product/all",(req,res)=>{
+    res.render('../views/product/products')
+})
+
+app.get("/product/:id",(req,res)=>{
+    res.render('../views/product/product.ejs')
+})
+
+app.get("/product/post",(req,res)=>{
+    res.render("../views/product/productForm.ejs");
+})
+
+
+
+
+app.use('*' , (req , res) => {
+    res.send("404 not found")
+})
+
+app.listen(PORT, ()=>{
+    console.log("Listening on PORT 3000")
+})
