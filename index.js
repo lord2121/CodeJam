dcb87651a1bd6e90c49433c6cb0d3dd13038dc31
lord2121 <53796515+lord2121@users.noreply.github.com>
2022-04-20 23:17:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const ejsMate = require("ejs-mate")
 const productRoutes = require('./routes/productRoutes.js')
 const productModel = require('./models/productModel.js');
 const passport = require("passport")
-const FacebookStrategy = require('passport-facebook').Strategy;
 
 
 
@@ -14,29 +13,6 @@ const app = express()
 app.engine('ejs' , ejsMate);
 app.set("view engine" , "ejs")
 app.use(express.static("public"))
-
-passport.use(new FacebookStrategy({
-    clientID: 366825112153917,
-    clientSecret: "537e5fbf5466b8032d793d802fbaf427",
-    callbackURL: "http://localhost:3000/auth/facebook/bravisimoi"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-));
-
-app.get('/auth/facebook',
-  passport.authenticate('facebook'));
-
-app.get('/auth/facebook/bravisimoi',
-  passport.authenticate('facebook', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
-  });
-
 
 app.get("/",(req,res)=>{
     res.render("index")
