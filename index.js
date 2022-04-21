f7bcba70c7684b81b4e794d40f871758dfb0822b
AndreiMaces <80191286+AndreiMaces@users.noreply.github.com>
2022-04-21 12:37:55 +0300

    Add Login Button

--- index.js
+++ index.js
@@ -15,50 +15,53 @@ const passportLocalMongoose = require("passport-local-mongoose");
 
 
 const app = express()
-app.engine('ejs' , ejsMate);
-app.set("view engine" , "ejs")
+app.engine('ejs', ejsMate);
+app.set("view engine", "ejs")
 app.use(express.static("public"))
 
 
 app.use(session({
-    secret: "Micul nostru secret",
-    resave : false,
-    saveUninitialized: false
+  secret: "Micul nostru secret",
+  resave: false,
+  saveUninitialized: false
 }))
 
 app.use(passport.initialize());
 app.use(passport.session());
 
- // mongoose.connect('mongodb://localhost:27017/UserDB')
+// mongoose.connect('mongodb://localhost:27017/UserDB')
 
 const userSchema = new Schema({
-    email: String,
-    password: String,
-    facebookId: String
+  email: String,
+  password: String,
+  facebookId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
 
-const User = mongoose.model('User',userSchema);
+const User = mongoose.model('User', userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(function(user, done) {
-    done(null, user);
-  });
-  
-  passport.deserializeUser(function(user, done) {
-    done(null, user);
-  });
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
 
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
 
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+})
 
 passport.use(new FacebookStrategy({
-    clientID: process.env.FACEBOOK_APP_ID,
-    clientSecret:process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/secrets"
-  },
-  function(accessToken, refreshToken, profile, cb) {
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: "http://localhost:3000/auth/facebook/secrets"
+},
+  function (accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ facebookId: profile.id }, function (err, user) {
       return cb(err, user);
     });
@@ -70,41 +73,32 @@ app.get('/auth/facebook',
 
 app.get('/auth/facebook/secrets',
   passport.authenticate('facebook', { failureRedirect: '/login' }),
-  function(req, res) {
-      console.log("GGGG")
-    // Successful authentication, redirect home.
+  function (req, res) {
     res.redirect('/');
   });
 
-app.get("/",(req,res)=>{
-    res.render("index")
+app.get("/", (req, res) => {
+  res.render("index")
 })
 
 
 
-app.get("/product/all",(req,res)=>{
-    res.render('../views/product/products')
+app.get("/product/all", (req, res) => {
+  res.render('../views/product/products')
 })
 
-app.get("/product/:id",(req,res)=>{
-    res.render('../views/product/product.ejs')
+app.get("/product/:id", (req, res) => {
+  res.render('../views/product/product.ejs')
 })
 
-app.get("/product/post",(req,res)=>{
-    res.render("../views/product/productForm.ejs");
+app.get("/product/post", (req, res) => {
+  res.render("../views/product/productForm.ejs");
 })
 
-app.get("/login",(req,res)=>{
-    res.render("../views/partials/login.ejs");
+app.use('*', (req, res) => {
+  res.send("404 not found")
 })
 
-
-
-
-app.use('*' , (req , res) => {
-    res.send("404 not found")
-})
-
-app.listen(PORT, ()=>{
-    console.log("Listening on PORT 3000")
+app.listen(PORT, () => {
+  console.log("Listening on PORT 3000")
 })
