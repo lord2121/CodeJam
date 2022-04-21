3ffc88279b1372c4b1790ec3410e6d3921e4a386
lord2121 <53796515+lord2121@users.noreply.github.com>
2022-04-21 09:39:38 +0300

    Am adaugat cookie-uri si logare cu facebook

--- index.js
+++ index.js
@@ -1,18 +1,80 @@
+require('dotenv').config();
 const express = require("express")
 const ejs = require("ejs")
 const PORT = 3000
 const mongoose = require("mongoose")
+const session = require("express-session");
 const ejsMate = require("ejs-mate")
 const productRoutes = require('./routes/productRoutes.js')
 const productModel = require('./models/productModel.js');
 const passport = require("passport")
-
+const FacebookStrategy = require('passport-facebook').Strategy;
+const Schema = mongoose.Schema;
+const findOrCreate = require('mongoose-findorcreate');
+const passportLocalMongoose = require("passport-local-mongoose");
 
 
 const app = express()
 app.engine('ejs' , ejsMate);
 app.set("view engine" , "ejs")
 app.use(express.static("public"))
+
+
+app.use(session({
+    secret: "Micul nostru secret",
+    resave : false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+ // mongoose.connect('mongodb://localhost:27017/UserDB')
+
+const userSchema = new Schema({
+    email: String,
+    password: String,
+    facebookId: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = mongoose.model('User',userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
+
+
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret:process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+      console.log("GGGG")
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
 
 app.get("/",(req,res)=>{
     res.render("index")
--- models/productModel.js
+++ models/productModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://localhost:27017/Products')
+ mongoose.connect('mongodb://localhost:27017/Products')
 
 const ProductSchema = new Schema({
     image:String,
