2e22c7c82cb914a652e218c9f4a6a9ecc078ec2a
AndreiMaces <80191286+AndreiMaces@users.noreply.github.com>
2022-05-11 20:09:35 +0300

    Add server error handle

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const productModel = require('../models/productModel.js');
 const FB = require("fb");
-const { isLoggedIn } = require("../middleware.js");
+const { isLoggedIn, catchAsync } = require("../middleware.js");
 const router = express.Router();
 const { getProducts, productForm, productFormPost, getProduct, deleteProduct } = require("../controllers/productController.js");
 const multer = require("multer");
@@ -13,10 +13,10 @@ router.get("/product/all", getProducts);
 
 router.get("/product/post", productForm);
 
-router.post("/product/post", isLoggedIn, upload.single('newImage'), productFormPost);
+router.post("/product/post", isLoggedIn, upload.single('newImage'), catchAsync(productFormPost));
 
 router.get("/product/:id", getProduct);
 
-router.delete("/product/:id", isLoggedIn, deleteProduct)
+router.delete("/product/:id", isLoggedIn, catchAsync(deleteProduct));
 
 module.exports = router
